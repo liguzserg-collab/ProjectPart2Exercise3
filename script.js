6f6f63baafff76c998ff329cdda87df1cf279534
LiguzovSergei <sergeiliguzov@gmail.com>
2026-02-26 17:43:06 -0600

    Initial commit

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
+const emblemClue1 = "Eagle";
+const emblemClue2 = "Laurel";
+const emblemClue3 = 7;
 
-//const shiftValue = 3;
-const randomDecimal = Math.random();
-const range = 33 - 3 + 1; 
-//Answer1 - to include both ends of our range, 3 and 33
+let locationStart = "";
 
-const randomInRange = randomDecimal * range;
-//Answer2 - Multiplying `randomDecimal` by `range` scales the decimal number so that it falls in the range from 0 to `range`
+if (emblemClue1 === "Eagle")
+{
+  locationStart = "Forum";
+}
+else if (emblemClue1 === "Lion")
+{
+  locationStart = "Colosseum";
+}
+else
+{
+  locationStart = "Villa";
+}
 
-const randomInt = Math.floor(randomInRange);
-//Answer3 - We use Math.floor() to ensure rounding down to the nearest integer, yielding an integer between 0 and range - 1
-
-const shiftValue = randomInt + 3;
-//Answer4 - randomInt is a number between 0 and range - 1 , adding 3 shifts this range up, resulting in numbers from 3 to 33 inclusive.
-
-
-
-
-const friend = "BRUTUS";
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-const firstLetter = friend[0];
-const index = alphabet.indexOf(firstLetter.toLowerCase());
-
-
-
-const alphabetLength = alphabet.length;
-const newIndex = (index + shiftValue) % alphabetLength;
-const encryptedFirstLetter = alphabet[newIndex];
+if (emblemClue2 === "Laurel" && locationStart === "Forum")
+{
+  locationStart += " of Augustus";
+}
+else if (emblemClue2 === "Grapes" || locationStart === "Villa")
+{
+  locationStart += " of Pompey";
+}
 
 
+switch (emblemClue3)
+{
+  case 7:
+    locationStart += " North";
+    break;
+  case 3:
+    locationStart += " South";
+    break;
+  case 9:
+    locationStart += " East";
+    break;
+  case 4:
+    locationStart += " West";
+    break;
+}
 
-const encryptedMessage = "EUXWXV";
-const teaserMessage = encryptedMessage.slice(0, 3);
+//Answer1 - 
+// == (double equals) performs type coercion
+// === (triple equals) checks both value and type
 
-console.log(encryptedFirstLetter);
-console.log(teaserMessage);
+
 
 
 
